15c15522f1f1172c1d3a121e493cb8dbfa47c05a
Matin-Sangabi <89559512+Matin-Sangabi@users.noreply.github.com>
2022-06-10 17:04:23 +0430

    search filter and btnsfilter and products and renderProducts added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,60 @@
+const searchFilter = document.querySelector(".search-filter");
+const parentProduct = document.querySelector(".grid-products");
+const filterBtns = document.querySelectorAll(".btn");
+
 
 let allProducts = [];
 
-const filter ={
-    searchITem : ""
+const filter = {
+  searchITem: "",
 };
 
-document.addEventListener("DOMContentLoaded" , ()=>{
-    axios.get("http://localhost:3000/items").
-    then((res) =>{
-        allProducts = res.data;
-        // render products on Dom 
-    }). 
-    catch((err) => console.log(err));
+document.addEventListener("DOMContentLoaded", () => {
+    searchFilter.value = "";
+  axios
+    .get("http://localhost:3000/items")
+    .then((res) => {
+      allProducts = res.data;
+      // render products on Dom
+      renderProducts(allProducts, filter);
+    })
+    .catch((err) => console.log(err));
 });
 
-function renderProducts(products , filter){
-    
+function renderProducts(products, filter) {
+  const productsFilter = products.filter((p) => {
+    return p.title.toLowerCase().includes(filter.searchITem.toLowerCase());
+  });
+  parentProduct.innerHTML = "";
+
+  productsFilter.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+    <div class="product-img">
+        <img src="${product.image}" alt="p-${product.class}-${product.id}">
+    </div>
+    <div class="product-content">
+        <span class="price">${product.price} $</span>
+        <span class="title">${product.title}</span>
+    </div>
+    <div class="product-btn">
+        <button type="button" class="product-btn">
+            <span>Add to cart</span>
+        </button>
+    </div>`;
+    parentProduct.appendChild(productDiv);
+  });
 }
+
+searchFilter.addEventListener("input", (e) => {
+  filter.searchITem = e.target.value;
+  renderProducts(allProducts, filter);
+});
+
+filterBtns.forEach((btn)=>{
+    btn.addEventListener("click" , (e)=>{
+        filter.searchITem =  e.target.dataset.filter;
+        renderProducts(allProducts , filter);
+    });
+});
